7e27934904584a595bacd455bf0b7400b22716be
Nathaniel Cook <nvcook42@gmail.com>
2023-03-09 10:02:32 -0700

    fix: remove irrelvant content loader changes

--- iroh-resolver/src/lib.rs
+++ iroh-resolver/src/lib.rs
@@ -1,7 +1,4 @@
 pub mod dns_resolver;
 pub mod resolver;
 
-pub use iroh_unixfs::content_loader::{
-    ContentLoader, FullLoader, FullLoaderConfig, LoaderFromProviders,
-};
 pub use resolver::{Path, PathType};
